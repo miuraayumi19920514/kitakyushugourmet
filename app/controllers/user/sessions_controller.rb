adc520fc0cd291d51109dbd957b167b5174648b5
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-10-07 05:40:31 +0000

    [add]user/sessionコントローラに退会機能（メアド・パスワードミスも）を実装

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,6 +1,31 @@
 # frozen_string_literal: true
 
 class User::SessionsController < Devise::SessionsController
+  before_action :user_state, only: [:create]
+  
+  
+  private
+
+    def user_state
+        user = User.find_by(email: params[:user][:email])
+        if user.nil?
+          flash[:alert] = "アカウントが見つかりません。新規会員登録を行ってください。"
+          redirect_to new_user_registration_path and return
+        end
+        
+        unless user.valid_password?(params[:user][:password])
+          flash[:alert] = "メールアドレス or パスワードが違います。"
+          redirect_to new_user_session_path and return
+        end
+        
+        if user.is_active == false
+          flash[:alert] = "退会済みです。新規会員登録を行ってください"
+          redirect_to new_user_registration_path
+        end
+    end
+  
+  
+  
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
