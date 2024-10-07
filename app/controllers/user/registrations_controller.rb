d0c01545b61dac186deb222e2e23071b71ee51dd
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-10-07 12:12:04 +0000

    [fix]adminのsessionsコントローラ、userのregistrations・sessionsコントローラにログイン後の遷移先を記述した

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class User::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   
+  def after_sign_up_path_for(resource)
+     reviews_path
+  end
   
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :local_person])
--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -3,7 +3,9 @@
 class User::SessionsController < Devise::SessionsController
   before_action :user_state, only: [:create]
 
-  
+   def after_sign_in_path_for(resource)
+     root_path
+   end
   
   private
 
