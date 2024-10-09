e7b385ac07420532dcca99d0e2cef2c67b4eced0
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-10-09 07:48:48 +0000

    [add]reviewのindex,show,editページの記述

--- app/controllers/user/reviews_controller.rb
+++ app/controllers/user/reviews_controller.rb
@@ -17,18 +17,32 @@ class User::ReviewsController < ApplicationController
   end
 
   def index
+    @reviews = Review.all.includes(:user)
   end
 
   def show
+    @review = Review.find(params[:id])
+    @user = @review.user
   end
   
   def edit
+    @review = Review.find(params[:id])
   end
   
   def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "編集しました。"
+      redirect_to review_path(@review)
+    else
+      render:@review
+    end
   end
   
   def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to mypage_path
   end
   
   
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,13 +2,13 @@ class Review < ApplicationRecord
   belongs_to :user
   has_many :comments ,dependent: :destroy
   has_many :favorites ,dependent: :destroy
-  
+
   validates :shop,  presence: true
   validates :address,  presence: true
   validates :title,  presence: true, length: { in: 5..30 }
   validates :body,  presence: true, length: { in: 2..400 }
   validates :star,  presence: true
-  
+
   has_one_attached :image
   def get_image(width, height)
     unless image.attached?
@@ -17,5 +17,6 @@ class Review < ApplicationRecord
     end
     image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
 end
+
