5b9b3262f8bebd36d12b8fff6b9ee4ce63182700
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-10-16 00:48:56 +0000

    [fix]reviewモデルにgenreカラムを追加。レビュー投稿フォームにgenre記入欄作成。各一覧にgenreを表示

--- db/migrate/20241007060914_create_reviews.rb
+++ db/migrate/20241007060914_create_reviews.rb
@@ -5,6 +5,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
       t.integer :user_id, null: false
       t.string :shop, null: false
       t.string :address, null: false
+      t.string :genre, null: false
       t.string :title, null: false
       t.text :body, null: false
       t.integer :star, null: false
